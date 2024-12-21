60dc2439bfb53f56308d1f948618b5fca5c7b6d6
Shivanshh369 <shivanshhh369@gmail.com>
2024-12-21 21:20:44 +0530

    update 4 at a time

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,14 +3,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-   const [counter, setCounter] = useState(15)
+   const [counter, setCounter] = useState(0)
   //let counter = 15
 
   const addValue = () => {
-    setCounter(counter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
   }
   const removeValue = () => {
-      setCounter(counter - 1);
+      setCounter((prevCounter) => prevCounter - 1);
+      setCounter((prevCounter) => prevCounter - 1);
+      setCounter((prevCounter) => prevCounter - 1);
+      setCounter((prevCounter) => prevCounter - 1);
 
     }
   
