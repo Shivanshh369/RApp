8ebe3d6e3dd6546f4e5f249b8e4e09ed5ebec7b5
Shivanshh369 <shivanshhh369@gmail.com>
2024-12-22 00:42:29 +0530

    tailwinf

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1596526131090-bcbe09e432d3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <h1 className='text-2xl bg-orange-200 p-3 rounded-md'>A card for Photo</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, beatae?</p>
+    </div>
+  )
+}
+
+export default Card
